3f3b17defdba99ac934d75910632056a37c709f4
rg-rupeee <rupeshgarhwal3920@gmail.com>
2024-07-21 15:27:25 +0530

    updating logger package

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,3 +1,38 @@
-export const log = (str: any) => {
-  console.log("logger: " + str);
+import winston, { LogEntry } from 'winston';
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
 };
+
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+const transports = [new winston.transports.Console()];
+
+const logger = winston.createLogger({
+  level: 'debug',
+  levels,
+  format,
+  transports,
+});
+
+export const stream = {
+  write: (message: string) => {
+    logger.info(message.substring(0, message.lastIndexOf('\n')));
+  },
+};
+
+export const log = (message: string) => {
+  logger.info(message);
+};
+
+export default logger;
